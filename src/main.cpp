62e54ac0f3c9f35ef252a5abb0cb15a1e1f4de9f
komppa <posti@rantakangas.com>
2023-07-06 14:53:03 +0300

    immplement relay control

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,9 @@
 // Fan control pin. 16 is D0 on esp8266. Also blinks the onboard LED.
 const int FAN_PWM_PIN = 16;
 
+// Relay control pin. 5 is D1 on esp8266.
+const int RELAY_PIN = 5;
+
 // WIFI credentials
 const char* WIFI_SSID = "";
 const char* WIFI_PASSWORD = "";
@@ -37,13 +40,45 @@ void callback(char* topic, byte* payload, unsigned int length) {
     for (int i = 0; i < length; i++) {
         Serial.print((char)payload[i]);
     }
-    Serial.println();
+
+    Serial.println("   length: " + String(length));
+    Serial.println("");
 
 
     if (length == 0 || length > 3) {
         Serial.println("Invalid payload length");
         return;
     }
+
+    // Check if message is 'on' or 'off'
+    if (
+        length == 2 && 
+        (
+            (payload[0] == 'o' && payload[1] == 'n') ||
+            (payload[0] == 'O' && payload[1] == 'N')
+        )
+    ) {
+        Serial.println("Turning on");
+        // Turn on the fan with a speed burst to get it started
+        analogWrite(FAN_PWM_PIN, 200);
+        digitalWrite(RELAY_PIN, 0);     // Turn on the relay
+        delay(750);
+        analogWrite(FAN_PWM_PIN, 30);   // "Normal" IDLE speed
+        return;
+    }
+
+    if (
+        length == 3 && 
+        (
+            (payload[0] == 'o' && payload[1] == 'f' && payload[2] == 'f') ||
+            (payload[0] == 'O' && payload[1] == 'F' && payload[2] == 'F')
+        )
+    ) {
+        Serial.println("Turning off");
+        digitalWrite(RELAY_PIN, 1);
+        return;
+    }
+
 
     // Check if the payload is a number
     for (int i = 0; i < length; i++) {
@@ -86,7 +121,7 @@ void reconnect() {
 
             // Tell that we are online
             client.publish("status", "online");
-            client.publish("version", "0.1");
+            client.publish("version", "0.2");
 
             // Subscribe to the control topic to receive commands
             // from the server to control the fan speed
@@ -105,7 +140,9 @@ void setup() {
     Serial.begin(9600);
 
     pinMode(FAN_PWM_PIN, OUTPUT);
+    pinMode(RELAY_PIN, OUTPUT);
     analogWrite(FAN_PWM_PIN, 30);
+    digitalWrite(RELAY_PIN, 1); // On startup, the relay is off
 
     // Since this project is meant to be used with ESP8266, we can use the
     // ESP32 to check if this is compiled for simulation therefore requiring
@@ -137,6 +174,8 @@ void setup() {
 }
 
 void loop() {
+
+    delay(10);
 
     #ifdef ESP8266
     ArduinoOTA.handle();
